d3c4f16d5d2d08f5e7e06dd5b07c747903dbde1b
Frank Mbappe <kevyndrews@gmail.com>
2020-03-14 14:30:48 +0100

    Initial commit

---
+++ app/src/main/java/com/gm/safedrive/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gm.safedrive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
